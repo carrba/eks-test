79453668b2be8456a451eddcb359600cbc60f79e
Brian Carr <b.carr@btinternet.com>
2023-01-05 10:25:07 +0000

    added eks nodegroup

--- lib/eks-test-stack.ts
+++ lib/eks-test-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
+// import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { lambda_layer_kubectl, Stack, StackProps } from 'aws-cdk-lib';
+import { aws_eks as eks } from 'aws-cdk-lib';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+import { KubectlLayer } from 'aws-cdk-lib/lambda-layer-kubectl';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class EksTestStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class EksTestStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const eksCluster = new eks.Cluster(this, 'HelloEKS', {
+      version: eks.KubernetesVersion.V1_21,
+      //: KubectlLayer.fromLayerVersionAttributes.
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EksTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const nodeGroup = new eks.Nodegroup(this, "NodeGroup1", {
+      cluster: eksCluster,
+      capacityType: eks.CapacityType.SPOT,
+      desiredSize: 1,
+      maxSize: 3,
+      minSize: 1,
+      amiType: eks.NodegroupAmiType.AL2_X86_64
+    })
+    }
 }
